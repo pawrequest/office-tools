ea8d9632b2dac49b3fc60d75285a2ea521561ac9
Paul <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-29 23:27:55 +0000

    pre-commit

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -10,20 +10,24 @@ from src.office_tools.o_tool import get_installed_combinations
 @pytest.fixture
 def temp_doc():
     with TemporaryDirectory() as temp_dir:
-        temp_file_l = Path(temp_dir) / 'test.docx'
+        temp_file_l = Path(temp_dir) / "test.docx"
         document = Document()
-        document.add_heading('Document Title', 0)
-        p = document.add_paragraph('Test Text')
-        p.add_run('bold run text').bold = True
+        document.add_heading("Document Title", 0)
+        p = document.add_paragraph("Test Text")
+        p.add_run("bold run text").bold = True
         document.save(temp_file_l)
 
         yield temp_file_l
 
 
 @pytest.mark.parametrize("office_tool_instance", get_installed_combinations())
-def test_office_tools(office_tool_instance, temp_doc):
+def test_installed_tools(office_tool_instance, temp_doc):
     print(office_tool_instance)
     doc_handler = office_tool_instance.doc
     pdf_file = doc_handler.to_pdf(temp_doc)
     assert pdf_file.exists()
     ...
+
+
+def test_smth():
+    assert 1 > 0
