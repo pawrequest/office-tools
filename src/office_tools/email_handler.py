5f2b38603f0b1095a4a89fd1f90362cd5f153717
Paul <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-30 02:37:13 +0000

    pre-commit, ruff

--- src/office_tools/email_handler.py
+++ src/office_tools/email_handler.py
@@ -67,5 +67,8 @@ def gmail_compose_link(email: Email) -> str:
     to_encoded = email.to_address.replace('@', '%40')
     subject_encoded = email.subject.replace(' ', '%20')
     body_encoded = email.body.replace(' ', '%20')
-    compose_link = f'https://mail.google.com/mail/u/0/?view=cm&fs=1&to={to_encoded}&su={subject_encoded}&body={body_encoded}'
+    compose_link = (
+        f'https://mail.google.com/mail/u/0/?view=cm&fs=1&to='
+        f'{to_encoded}&su={subject_encoded}&body={body_encoded}'
+    )
     return compose_link
